6e14febc5623e9b9f0e4d17a0f5d3f860aa4afe2
Dannon Baker <dannon.baker@gmail.com>
2016-04-20 11:16:21 -0400

    Stripping custom remoteuser toolshed middleware, using standard.

--- lib/galaxy/webapps/tool_shed/buildapp.py
+++ lib/galaxy/webapps/tool_shed/buildapp.py
@@ -195,11 +195,13 @@ def wrap_in_middleware( app, global_conf, **local_conf ):
     # protects Galaxy from improperly configured authentication in the
     # upstream server
     if asbool(conf.get( 'use_remote_user', False )):
-        from galaxy.webapps.tool_shed.framework.middleware.remoteuser import RemoteUser
+        from galaxy.web.framework.middleware.remoteuser import RemoteUser
         app = RemoteUser( app, maildomain=conf.get( 'remote_user_maildomain', None ),
                           display_servers=util.listify( conf.get( 'display_servers', '' ) ),
                           admin_users=conf.get( 'admin_users', '' ).split( ',' ),
-                          remote_user_secret_header=conf.get('remote_user_secret', None) )
+                          remote_user_header=conf.get( 'remote_user_header', 'HTTP_REMOTE_USER' ),
+                          remote_user_secret_header=conf.get('remote_user_secret', None),
+                          normalize_remote_user_email=conf.get('normalize_remote_user_email', False))
         log.debug( "Enabling 'remote user' middleware" )
     # The recursive middleware allows for including requests in other
     # requests or forwarding of requests, all on the server side.
