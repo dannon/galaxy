6b25f1f955cb228f98786176975de0d5354da293
Dannon Baker <dannon.baker@gmail.com>
2018-01-05 12:30:50 -0500

    Drop context deepcopy in visitInputs

--- client/galaxy/scripts/mvc/form/form-data.js
+++ client/galaxy/scripts/mvc/form/form-data.js
@@ -200,7 +200,6 @@ export var matchCase = (input, value) => {
  * @param{dict}   callback  - Called with the mapped dictionary object and corresponding model node
  */
 export var visitInputs = (inputs, callback, prefix, context) => {
-    context = $.extend(true, {}, context);
     _.each(inputs, input => {
         if (input && input.type && input.name) {
             context[input.name] = input;
