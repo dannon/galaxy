d0c22ee3da21c5bb7860953ebed10b187dd8b24f
Dannon Baker <dannon.baker@gmail.com>
2019-04-01 21:20:10 -0400

    Test a simple toast lib swap

--- client/galaxy/scripts/ui/toast.js
+++ client/galaxy/scripts/ui/toast.js
@@ -1,7 +1,62 @@
-import Toastr from "toastr";
+import "scss/toast.scss";
 
-// Simple passthrough for now, but this gives us an extension point to work
-// against when converting over to either boostrap-specific or vue-based
-// toasts.
+import Vue from "vue";
+import Toasted from "vue-toasted";
 
-export const Toast = Toastr;
+const toastOptions = {
+    base: {
+        iconPack: "fontawesome",
+        duration: 5000,
+        action: {
+            text: "close",
+            onClick: (e, t) => {
+                t.goAway(0);
+            }
+        }
+        //theme: "bubble",
+    },
+    success: {
+        type: "success",
+        icon: "check"
+    },
+    info: {
+        type: "info",
+        icon: "info"
+    },
+    error: {
+        type: "error",
+        icon: "times"
+    },
+    warning: {
+        type: "warning",
+        icon: "warning"
+    }
+};
+
+export class VueToasted {
+    constructor(options) {
+        Vue.use(Toasted, toastOptions);
+    }
+    toast(msg, options = {}) {
+        options = { ...toastOptions.base, ...options };
+        Vue.toasted.show(msg, options);
+    }
+    success(msg, options = {}) {
+        options = { ...toastOptions.success, ...options };
+        return this.toast(msg, options);
+    }
+    info(msg, options = {}) {
+        options = { ...toastOptions.info, ...options };
+        return this.toast(msg, options);
+    }
+    error(msg, options = {}) {
+        options = { ...toastOptions.error, ...options };
+        return this.toast(msg, options);
+    }
+    warning(msg, options = {}) {
+        options = { ...toastOptions.warning, ...options };
+        return this.toast(msg, options);
+    }
+}
+
+export const Toast = new VueToasted();
