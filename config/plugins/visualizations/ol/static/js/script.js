8016a885459894a1fd5a17993b388e6228ecc66b
Anup Kumar <anup.rulez@gmail.com>
2019-05-02 16:14:27 +0200

    Read GeoJSOn files

--- config/plugins/visualizations/ol/static/js/script.js
+++ config/plugins/visualizations/ol/static/js/script.js
@@ -1,65 +1,168 @@
-var tile = new ol.layer.Tile({source: new ol.source.OSM()});
-var source = new ol.source.Vector({wrapX: false});
-var vector = new ol.layer.Vector({source: source});
-var scaleLineControl = new ol.control.ScaleLine();
-var fullScreen = new ol.control.FullScreen();
 
-var map = new ol.Map({
-    controls: ol.control.defaults().extend([scaleLineControl, fullScreen]),
-    layers: [tile, vector],
-    target: 'map-view',
-    view: new ol.View({
-      center: [-11000000, 4600000],
-      zoom: 4
-    })
-});
+var MapViewer = (function(mv) {
 
-var graticule = new ol.Graticule({
-    strokeStyle: new ol.style.Stroke({
-        color: 'rgba(255,120,0,0.9)',
-        width: 2,
-        lineDash: [0.5, 4]
-    }),
-    showLabels: true
-});
+    mv.setInteractions = function(map, source) {
+    
+        var typeSelect = document.getElementById('geometry-type');
+        var draw;
 
-graticule.setMap(map);
+        var addInteraction = () => {
+            let value = typeSelect.value;
+            if (value !== 'None') {
+                draw = new ol.interaction.Draw({
+                    source: source,
+                    type: typeSelect.value,
+                    freehand: true
+                });
+                map.addInteraction(draw);
+            }
+        };
 
-var typeSelect = document.getElementById('geometry-type');
+        typeSelect.onchange = () => {
+           map.removeInteraction(draw);
+           addInteraction();
+        };
 
-var draw;
-
-var addInteraction = () => {
-    let value = typeSelect.value;
-    if (value !== 'None') {
-        draw = new ol.interaction.Draw({
-            source: source,
-            type: typeSelect.value,
-            freehand: true
-        });
-        map.addInteraction(draw);
-    }
-}
-
-typeSelect.onchange = () => {
-    map.removeInteraction(draw);
-    addInteraction();
-};
-
-addInteraction();
-
-document.getElementById('export-png').addEventListener('click', e => {
-    map.once('rendercomplete', event => {
-        let canvas = event.context.canvas;
-        let fileName = Math.random().toString(11).replace('0.', '');
-        fileName += '.png';
-        if (navigator.msSaveBlob) {
-            navigator.msSaveBlob(canvas.msToBlob(), fileName);
-        } else {
-            canvas.toBlob(blob => {
-                saveAs(blob, fileName);
+        addInteraction();
+    };
+    
+    mv.exportMap = function(map) {
+        document.getElementById('export-png').addEventListener('click', e => {
+            map.once('rendercomplete', event => {
+                let canvas = event.context.canvas;
+                let fileName = Math.random().toString(11).replace('0.', '');
+                fileName += '.png';
+                if (navigator.msSaveBlob) {
+                    navigator.msSaveBlob(canvas.msToBlob(), fileName);
+                } else {
+                    canvas.toBlob(blob => {
+                        saveAs(blob, fileName);
+                    });
+                }
             });
-        }
-    });
-    map.renderSync();
-});
+            map.renderSync();
+        });
+    };
+    
+    mv.loadFile = function(filePath) {
+
+        var tile = new ol.layer.Tile({source: new ol.source.OSM()});
+        var fullScreen = new ol.control.FullScreen();     
+        
+        var styles = {
+            'Polygon': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+            }),
+            'Circle': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+            }),
+            'Point': new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 5,
+                    fill: null,
+                    stroke: new ol.style.Stroke({color: 'red', width: 1})
+                })
+            }),
+            'LineString': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                })
+            }),
+            'MultiLineString': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                })
+            }),
+            'MultiPoint': new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 5,
+                    fill: null,
+                    stroke: new ol.style.Stroke({color: 'red', width: 1})
+                })
+            }),
+            'MultiPolygon': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 0, 255, 0.1)'
+                })
+            }),
+            'GeometryCollection': new ol.style.Style({
+                stroke: new ol.style.Stroke({
+                    color: 'red',
+                    width: 1
+                }),
+                fill: new ol.style.Fill({
+                    color: 'red'
+                }),
+                image: new ol.style.Circle({
+                    radius: 10,
+                    fill: null,
+                    stroke: new ol.style.Stroke({
+                        color: 'red'
+                    })
+                })
+            }),
+        };
+        
+        var styleFunction = function(feature) {
+            return styles[feature.getGeometry().getType()];
+        };
+        
+        var vectorSource = new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            url: filePath
+        })
+        
+        var vectorLayer = new ol.layer.Vector({
+            source: vectorSource,
+            style: styleFunction
+        });
+        
+        var scaleLineControl = new ol.control.ScaleLine();
+    
+        var map = new ol.Map({
+            controls: ol.control.defaults().extend([scaleLineControl, fullScreen]),
+            layers: [tile, vectorLayer],
+            target: 'map-view',
+            view: new ol.View({
+                center: [0, 0],
+                zoom: 2
+            })
+        });
+
+        var graticule = new ol.Graticule({
+            strokeStyle: new ol.style.Stroke({
+                color: 'rgba(255,120,0,0.9)',
+                width: 2,
+                lineDash: [0.5, 4]
+            }),
+            showLabels: true
+        });
+
+        graticule.setMap(map);
+        
+        mv.setInteractions(map, vectorSource);
+        mv.exportMap(map);
+    };
+
+    return mv;
+}(MapViewer || {}));
+
+console.log(ol);
--- config/plugins/visualizations/ol/templates/ol.mako
+++ config/plugins/visualizations/ol/templates/ol.mako
@@ -11,8 +11,11 @@
     ${h.stylesheet_link(app_root + 'css/font-awesome.min.css')}
     ${h.stylesheet_link(app_root + 'css/ol.css')}
     ${h.stylesheet_link(app_root + 'css/map_view.css')}
+    ${h.javascript_link( app_root + "js/jquery.min.js")}
     ${h.javascript_link( app_root + "js/ol.js")}
     ${h.javascript_link( app_root + "js/FileSaver.min.js")}
+    ${h.javascript_link( app_root + "js/shpgeojson.js")}
+    
 </head>
 <body class="body-map-view">
     <div id="map-view" class="map"></div>
@@ -27,6 +30,11 @@
         <button id="export-png" type="button" class="btn btn-secondary btn-sm"><i class="fa fa-download"></i>Export</button>
     </div>
     ${h.javascript_link( app_root + "js/script.js")}
+    <script>
+        $(document).ready(function() {
+            MapViewer.loadFile(`${h.url_for(controller='/datasets', action='index')}/${hda.id}/display`);
+        });
+    </script>
 </body>
 </html>
 
