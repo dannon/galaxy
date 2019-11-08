e83479c757e19cb407242ef5ef05f654a79c850b
Juleen Graham <juleengrah@gmail.com>
2019-11-08 10:34:54 -0500

    user preferences
    - add external id management to userpreferences vue file
    - standardise capitalisation of titles (some had camel case, some sentence case, others followed neither rule)

--- client/galaxy/scripts/components/User/UserPreferencesModel.js
+++ client/galaxy/scripts/components/User/UserPreferencesModel.js
@@ -6,7 +6,7 @@ export const getUserPreferencesModel = () => {
     const config = Galaxy.config;
     return {
         information: {
-            title: _l("Manage information"),
+            title: _l("Manage Information"),
             description: "Edit your email, addresses and custom parameters or change your public name.",
             url: `api/users/${Galaxy.user.id}/information/inputs`,
             icon: "fa-user",
@@ -14,7 +14,7 @@ export const getUserPreferencesModel = () => {
             shouldRender: !config.use_remote_user
         },
         password: {
-            title: _l("Change password"),
+            title: _l("Change Password"),
             description: _l("Allows you to change your login credentials."),
             icon: "fa-unlock-alt",
             url: `api/users/${Galaxy.user.id}/password/inputs`,
@@ -22,8 +22,15 @@ export const getUserPreferencesModel = () => {
             redirect: "user",
             shouldRender: !config.use_remote_user
         },
+        external_ids: {
+            title: _l("Manage Third-Party Identities"),
+            description: _l("Connect or disconnect access to your third-party identities."),
+            icon: "fa-id-card-o",
+            submit_title: "Disconnect identity",
+            submit_icon: "fa-trash"
+        },
         communication: {
-            title: _l("Change communication settings"),
+            title: _l("Change Communication Settings"),
             description: _l("Enable or disable the communication feature to chat with other users."),
             url: `api/users/${Galaxy.user.id}/communication/inputs`,
             icon: "fa-comments-o",
@@ -31,7 +38,7 @@ export const getUserPreferencesModel = () => {
             shouldRender: !!config.enable_communication_server
         },
         permissions: {
-            title: _l("Set dataset permissions for new histories"),
+            title: _l("Set Dataset Permissions for New Histories"),
             description:
                 "Grant others default access to newly created histories. Changes made here will only affect histories created after these settings have been stored.",
             url: `api/users/${Galaxy.user.id}/permissions/inputs`,
@@ -40,12 +47,12 @@ export const getUserPreferencesModel = () => {
             redirect: "user"
         },
         make_data_private: {
-            title: _l("Make all data private"),
+            title: _l("Make All Data Private"),
             description: _l("Click here to make all data private."),
             icon: "fa-lock"
         },
         api_key: {
-            title: _l("Manage API key"),
+            title: _l("Manage API Key"),
             description: _l("Access your current API key or create a new one."),
             url: `api/users/${Galaxy.user.id}/api_key/inputs`,
             icon: "fa-key",
@@ -60,7 +67,7 @@ export const getUserPreferencesModel = () => {
             submit_icon: "fa-check"
         },
         toolbox_filters: {
-            title: _l("Manage Toolbox filters"),
+            title: _l("Manage Toolbox Filters"),
             description: _l("Customize your Toolbox by displaying or omitting sets of Tools."),
             url: `api/users/${Galaxy.user.id}/toolbox_filters/inputs`,
             icon: "fa-filter",
@@ -69,17 +76,17 @@ export const getUserPreferencesModel = () => {
             shouldRender: !!config.has_user_tool_filters
         },
         custom_builds: {
-            title: _l("Manage custom builds"),
+            title: _l("Manage Custom Builds"),
             description: _l("Add or remove custom builds using history datasets."),
             icon: "fa-cubes"
         },
         genomespace: {
-            title: _l("Request GenomeSpace token"),
+            title: _l("Request GenomeSpace Token"),
             description: _l("Requests token through OpenID."),
             icon: "fa-openid"
         },
         logout: {
-            title: _l("Sign out"),
+            title: _l("Sign Out"),
             description: _l("Click here to sign out of all sessions."),
             icon: "fa-sign-out",
             shouldRender: !!Galaxy.session_csrf_token
