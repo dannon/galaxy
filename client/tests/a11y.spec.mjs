79d8e2385417539d5f3e29281dfd82797cd1bb63
Dannon Baker <dannon.baker@gmail.com>
2021-10-12 10:21:44 -0400

    Add static wait to force page load -- need a better way to do this. I thought waiting for domcontentloaded and *then* networkidle would work, but maybe we just wait for the masthead or something?

--- client/tests/a11y.spec.mjs
+++ client/tests/a11y.spec.mjs
@@ -4,6 +4,8 @@ import { injectAxe, checkA11y } from "axe-playwright";
 test("basic test", async ({ page }) => {
     await page.goto("http://localhost:8080");
     await injectAxe(page);
+    await page.waitForTimeout(1000); // Sadly required.
+    await page.waitForLoadState("domcontentloaded"); // wait for the page to fire domcontentloaded
     await page.waitForLoadState("networkidle");
     await checkA11y(page, null, {
         detailedReport: true,
