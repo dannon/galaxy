62efaa4ca6aee5e33bfb6d3160dd1f281164ff07
Dannon Baker <dannon.baker@gmail.com>
2021-11-10 16:20:59 -0500

    Tweak base playwright configuration

--- client/playwright.config.js
+++ client/playwright.config.js
@@ -1,7 +1,11 @@
 const config = {
     // Each test is given 30 seconds
+    use: {
+        baseUrl: "http://localhost:8080",
+        headless: true,
+        viewport: { width: 1280, height: 720 },
+    },
     timeout: 30000,
-
     testMatch: "**/*.spec.mjs",
 };
 
