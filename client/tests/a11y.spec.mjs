3dec9d9d1e41f2d136d01515be4a450d70822c4b
Dannon Baker <dannon.baker@gmail.com>
2021-11-10 16:20:36 -0500

    Add login a11y test

--- client/tests/a11y.spec.mjs
+++ client/tests/a11y.spec.mjs
@@ -1,13 +1,29 @@
 import { test } from "@playwright/test";
 import { injectAxe, checkA11y } from "axe-playwright";
 
-test("basic test", async ({ page }) => {
+test("Home route a11y test", async ({ page }) => {
     await page.goto("http://localhost:8080");
     await injectAxe(page);
     await page.waitForTimeout(1000); // Sadly required.
     await page.waitForLoadState("domcontentloaded"); // wait for the page to fire domcontentloaded
     await page.waitForLoadState("networkidle");
     await checkA11y(page, null, {
+        includedImpacts: ["critical", "serious"],
+        detailedReport: true,
+        detailedReportOptions: {
+            html: true,
+        },
+    });
+});
+
+test("Login a11y test", async ({ page }) => {
+    await page.goto("http://localhost:8080/login");
+    await injectAxe(page);
+    await page.waitForTimeout(1000); // Sadly required.
+    await page.waitForLoadState("domcontentloaded"); // wait for the page to fire domcontentloaded
+    await page.waitForLoadState("networkidle");
+    await checkA11y(page, null, {
+        includedImpacts: ["critical", "serious"],
         detailedReport: true,
         detailedReportOptions: {
             html: true,
