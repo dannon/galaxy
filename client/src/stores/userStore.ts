25e38e2967979e0a4c69045a94542538e1c0dd95
Dannon Baker <dannon.baker@gmail.com>
2023-11-11 09:15:33 -0500

    bug: userStore set favorite tools.  typescript flagged.

--- client/src/stores/userStore.ts
+++ client/src/stores/userStore.ts
@@ -124,7 +124,7 @@ export const useUserStore = defineStore("userStore", () => {
 
     function setFavoriteTools(tools: string[]) {
         if (currentPreferences.value) {
-            currentPreferences.value.favorites.tools = tools;
+            currentPreferences.value.favorites.tools = tools ?? [];
         }
     }
 
