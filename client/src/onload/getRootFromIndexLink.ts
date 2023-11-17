006cfd1abb29663f7ca05ff385585bf0b76075b9
Dannon Baker <dannon.baker@gmail.com>
2023-11-17 11:24:03 -0500

    Make getRootFromIndexLink a bit more robust.
    This was getting pulled into the webworker context and was blowing up
    with document being undefined.  We should also follow up with
    restructuring so that it is not attempted to be used from the web worker
    context, however.

--- client/src/onload/getRootFromIndexLink.ts
+++ client/src/onload/getRootFromIndexLink.ts
@@ -1,15 +1,23 @@
 import { serverPath } from "@/utils/serverPath";
 
 /**
- * Finds <link rel="index"> in head element and pulls root url fragment from
- * there That should probably be a <base> tag instead since that's how
- * they're using <link rel="index" />
+ * Extracts the root URL from the <link rel="index"> tag in the document head.
+ * If not present, defaults to a specified root URL.
  *
- * @param {string} [defaultRoot="/"]
- * @returns {string}
+ * We should shift to using the <base> tag standard for defining the root URL.
+ *
+ * @param {string} [defaultRoot="/"] - The default root URL to use if no index link is found.
+ * @returns {string} The root URL.
  */
 export function getRootFromIndexLink(defaultRoot = "/"): string {
-    const links = document.getElementsByTagName("link");
-    const indexLink = Array.from(links).find((link) => link.rel == "index");
-    return indexLink && indexLink.href ? serverPath(indexLink.href) : defaultRoot;
+    // Early return if not in a browser environment
+    if (typeof document === "undefined") {
+        return defaultRoot;
+    }
+
+    // Search for the <link rel="index"> tag
+    const indexLink = Array.from(document.getElementsByTagName("link")).find((link) => link.rel === "index");
+
+    // Return the href of the index link if it exists, otherwise default root
+    return indexLink?.href ? serverPath(indexLink.href) : defaultRoot;
 }
