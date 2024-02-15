ce50d5aa0b17a6a9f46318c7e2a21c2030ae8d8d
Dannon Baker <dannon.baker@gmail.com>
2024-02-15 15:54:58 -0500

    Prettier configuration change to cjs, run it

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,29 @@
-import vue from '@vitejs/plugin-vue2'; // For Vue 2
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import vue from "@vitejs/plugin-vue2"; // For Vue 2
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tsconfigPaths(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // Do we need includePaths?
-      },
+    plugins: [vue(), tsconfigPaths()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Do we need includePaths?
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-        '@': path.resolve(__dirname, './src'),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  build: {
-    manifest: 'true',
-    rollupOptions: {
-      minify: 'terser',
-      input: 'src/entry/analysis/index.ts',
+    build: {
+        manifest: "true",
+        rollupOptions: {
+            minify: "terser",
+            input: "src/entry/analysis/index.ts",
+        },
+        cssCodeSplit: true,
+        sourcemap: true,
     },
-    cssCodeSplit: true,
-    sourcemap: true,
-  },
 });
