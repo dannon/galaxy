e3826ac19974fef03ca5d6e0cd04988d6acd22d1
Dannon Baker <dannon.baker@gmail.com>
2024-10-18 16:30:33 -0400

    Clean up vite.config formatting/imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import vue from '@vitejs/plugin-vue2'; // For Vue 2
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
+import vue from "@vitejs/plugin-vue2"; // For Vue 2
+import path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const targetEnv = process.env.NODE_ENV == "production" ? "production" : "development";
 
@@ -9,34 +9,28 @@ const scriptsBase = path.resolve(__dirname, "./src");
 const styleBase = path.join(scriptsBase, "style");
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    tsconfigPaths(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-        includePaths: [
-          path.join(styleBase, "scss"),
-          path.resolve(__dirname, "./node_modules"),
-        ],
-      },
+    plugins: [vue(), tsconfigPaths()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                includePaths: [path.join(styleBase, "scss"), path.resolve(__dirname, "./node_modules")],
+            },
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      config: path.resolve(scriptsBase, "config", targetEnv) + ".js",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            config: path.resolve(scriptsBase, "config", targetEnv) + ".js",
+        },
     },
-  },
-  build: {
-    manifest: true,
-    minify: 'terser',
-    rollupOptions: {
-      input: 'src/entry/analysis/index.ts',
+    build: {
+        manifest: true,
+        minify: "terser",
+        rollupOptions: {
+            input: "src/entry/analysis/index.ts",
+        },
+        cssCodeSplit: true,
+        sourcemap: true,
     },
-    cssCodeSplit: true,
-    sourcemap: true,
-  },
 });
