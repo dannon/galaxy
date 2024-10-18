2f64ab561f40b5efec1f25ae20eed2800d28411e
Dannon Baker <dannon.baker@gmail.com>
2024-10-18 14:44:59 -0400

    Prefix ui imports

--- client/src/components/Collections/ListCollectionCreator.vue
+++ client/src/components/Collections/ListCollectionCreator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import "ui/hoverhighlight";
+import "@/ui/hoverhighlight";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSortAlphaDown, faUndo } from "@fortawesome/free-solid-svg-icons";
